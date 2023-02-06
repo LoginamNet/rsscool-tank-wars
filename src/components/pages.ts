ea1324f15f158edf2b7456c11ee943993c602a41
vigo44 <vvigo@rambler.ru>
2023-02-07 00:20:53 +0400

    refactor: remove property width, height from class page

--- src/components/pages.ts
+++ src/components/pages.ts
@@ -3,13 +3,11 @@ import { checkedQuerySelector } from './utils';
 
 export class Page {
     body = checkedQuerySelector(document, 'body');
-    width = CANVAS_WIDTH;
-    height = CANVAS_HEIGHT;
 
     renderGame() {
         this.body.innerHTML = `
         <div class="game">
-            <canvas class="canvas" width="${this.width}" height="${this.height}"></canvas>
+            <canvas class="canvas" width="${CANVAS_WIDTH}" height="${CANVAS_HEIGHT}"></canvas>
         </div>
         <div id= "game-data">
             <div class="angle" >Angle: 45</div>
