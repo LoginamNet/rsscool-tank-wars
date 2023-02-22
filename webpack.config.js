16708bcc26e3299a8e969ecceac491396bd878fc
LoginamNet <loginamnet@gmail.com>
2023-02-22 08:53:04 +0300

    feat: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ const baseConfig = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
+            favicon: './src/assets/favicon.ico',
         }),
         new CleanWebpackPlugin(),
         new ESLintPlugin({ extensions: ['ts', 'js'] }),
